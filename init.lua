d42e1de1b2a8a6e804e1961dbcd7b9389966396e
Louay Cheikh <brotherlu@gmail.com>
2024-07-22 12:05:11 -0400

    Switched to <M-Tab> for auto complete

--- init.lua
+++ init.lua
@@ -912,7 +912,7 @@ require('lazy').setup({
   },
 })
 
-vim.keymap.set('i', '<tab>', 'copilot#Accept("\\<CR>")', {
+vim.keymap.set('i', '<M-Tab>', 'copilot#Accept("\\<CR>")', {
   expr = true,
   replace_keycodes = false,
 })
